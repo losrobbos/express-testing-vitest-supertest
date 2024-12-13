c2a401364667bddc81702b1d379587d7a7457d44
losrobbos <losrobbos@web.de>
2024-12-13 10:28:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Test Driven Development (TDD)
+
+Testing Tool: Vitest
+`npm i -D vitest`
+
+Routes Testing: Supertest
+`npm i -D supertest`
+
+Tests starten:
+`npm test`
+
---
+++ tests/server.test.js
@@ -0,0 +1,45 @@
+import { describe, expect, test } from "vitest";
+import { app } from "../server";
+import supertest from "supertest";
+
+const request = supertest(app);
+
+// TEST SUITE => Tests die zusammengehören => Route Tests
+describe("Route Tests", () => {
+  test("Home Route", async () => {
+    const response = await request.get("/");
+    // testing
+    expect(response.statusCode).toBe(200);
+    // expect(response.text).toBe("Hello from API")
+    expect(response.text).toMatch(/jörgPI/);
+  });
+
+  test("Movies Route", async () => {
+    const response = await request.get("/movies");
+    expect(response.statusCode).toBe(200);
+  });
+
+  test("Movies Route", async () => {
+    const response = await request.get("/doesnotexist");
+    expect(response.statusCode).toBe(404);
+  });
+
+  test("Movie create", async () => {
+    const movieNew = {
+      title: "Nur den schönen Film",
+      rating: 9.7,
+    };
+    const response = await request.post("/movies").send(movieNew);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeDefined();
+    expect(response.body.title).toBe(movieNew.title);
+    expect(response.body._id).toBeTypeOf("string");
+  });
+
+  test("Movie create without data", async () => {
+    const response = await request.post("/movies").send({ });
+    expect(response.statusCode).toBe(400)
+    expect(response.body.error).toMatch(/Fuck off/) // valid error message
+  });
+});
